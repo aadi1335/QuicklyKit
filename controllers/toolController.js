990234e256306ee902b6507045b43395a130163f
aadi1335 <122515467+aadi1335@users.noreply.github.com>
2025-01-13 11:56:25 +0530

    1.1.0: version update

--- controllers/toolController.js
+++ controllers/toolController.js
@@ -1,29 +1,109 @@
 const expess = require('express');
 
 exports.getQr = (req, res) => {
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
     res.render('../views/tools/QrCode');
 }
 
 exports.getTinyUrl = (req, res) => {
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
     res.render('../views/tools/TinyUrl');
 }
 
 exports.getColorPicker = (req, res) => {
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
     res.render('../views/tools/ColorPicker');
 }
 
 exports.getIpFinder = (req, res) => {
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
     res.render('../views/tools/IPFinder');
 }
 
 exports.getTextToSpeech = (req, res) => {
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
     res.render('../views/tools/textToSpeech');
 }
 
 exports.getSpeechToText = (req, res) => {
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
     res.render('../views/tools/speechToText');
 }
 
 exports.getBmiCalc = (req, res) => {
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
     res.render('../views/tools/bmiCalc');
+}
+
+exports.getPdfTool = (req, res) =>{
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
+    res.status(404).json({message: "Page Not found...Maybe its under development"});
+}
+
+exports.getImageResizer = (req, res) =>{
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
+    res.status(404).json({message: "Page Not found...Maybe its under development"});
+}
+
+exports.getCurrencyConverter = (req, res) =>{
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
+    res.status(404).json({message: "Page Not found...Maybe its under development"});
+}
+
+exports.getMassConverter = (req, res) =>{
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
+    res.status(404).json({message: "Page Not found...Maybe its under development"});
+}
+
+exports.getRandomWheel = (req, res) =>{
+    const authToken = req.cookies;
+    const ispresent = 'AuthenticationString' in authToken;
+    if(!ispresent) {
+        return res.redirect('/Login');
+    }
+    res.status(404).json({message: "Page Not found...Maybe its under development"});
 }
--- routes/toolRoutes.js
+++ routes/toolRoutes.js
@@ -9,5 +9,10 @@ router.get('/tools/ip-finder', toolController.getIpFinder);
 router.get('/tools/text-to-speech', toolController.getTextToSpeech);
 router.get('/tools/speech-to-text', toolController.getSpeechToText);
 router.get('/tools/bmi-calculator', toolController.getBmiCalc);
+router.get('/tools/pdf-tools', toolController.getPdfTool);
+router.get('/tools/image-compressor', toolController.getImageResizer);
+router.get('/tools/currency-converter', toolController.getCurrencyConverter);
+router.get('/tools/mass-converter', toolController.getMassConverter);
+router.get('/tools/random-wheel', toolController.getRandomWheel);
 
 module.exports = router
